79056c6ec1006d1b0ed980d3d93e5f1fa2d67d0a
Wyatt Holliday <blueholliday@gmail.com>
2020-12-07 17:38:00 -0500

    finished assignment

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    Transform target;
+    NavMeshAgent agent;
+    int currentNode;
+    int previousNode;
+    public int enemyNum;
+
+    public enum EnemyState
+    {
+        patrol,
+        chase
+    };
+
+    EnemyState enemyState = EnemyState.patrol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if(enemyNum == 1)
+        {
+            currentNode = Random.Range(0, GameManager.gm.nodes.Length);
+        }
+        else if (enemyNum == 2)
+        {
+            currentNode = Random.Range(0, GameManager.gm.nodes2.Length);
+        }
+        previousNode = currentNode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(enemyState)
+        {
+            case EnemyState.patrol: Patrol();  break;
+            case EnemyState.chase: Chase();  break;
+            default: break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "node")
+        {
+            if (enemyNum == 1)
+            {
+            currentNode = Random.Range(0, GameManager.gm.nodes.Length);
+            while (currentNode == previousNode)
+            {
+                currentNode = Random.Range(0, GameManager.gm.nodes.Length);
+            }
+            previousNode = currentNode;
+            }
+            else if (enemyNum == 2)
+            {
+                currentNode = Random.Range(0, GameManager.gm.nodes2.Length);
+                while (currentNode == previousNode)
+                {
+                    currentNode = Random.Range(0, GameManager.gm.nodes2.Length);
+                }
+                previousNode = currentNode;
+            }
+        }
+
+        if(other.tag == "Player")
+        {
+            enemyState = EnemyState.chase;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            enemyState = EnemyState.patrol;
+        }
+    }
+
+    void Patrol()
+    {
+        if(enemyNum == 1)
+        {
+           agent.destination = GameManager.gm.nodes[currentNode].position;
+        }
+        else if (enemyNum == 2)
+        {
+           agent.destination = GameManager.gm.nodes2[currentNode].position;
+        }
+    }
+
+    void Chase()
+    {
+        agent.destination = GameManager.gm.player.transform.position;
+    }
+}
